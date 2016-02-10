89d99cc8ff6116b91898a2570b413dc32f3b0d85
Laurent Maillet <laurent.maillet@zenika.com>
2016-02-10 17:02:05 +0100

    add tp2 move tp1 test in tp2

--- Exercices/corrections/01-start-angular2/src/app/application.spec.ts
+++ Exercices/corrections/01-start-angular2/src/app/application.spec.ts
@@ -3,19 +3,15 @@ import {Application} from '../app/application';
 
 beforeEachProviders(() => [Application]);
 
-describe('App: Tedt', () => {
-  it('should have the `defaultMeaning` as 42', inject([Application], (app) => {
-    expect(app.defaultMeaning).toBe(42);
-  }));
-
-  it('should have the `helloMsg` with the right value', inject([Application], (app) => {
-    expect(app.helloMsg).toBe('This is my first component');
-  }));
-
-  describe('#meaningOfLife', () => {
-    it('should get the meaning of life', inject([Application], (app) => {
-      expect(app.meaningOfLife()).toBe('The meaning of life is 42');
-      expect(app.meaningOfLife(22)).toBe('The meaning of life is 22');
+describe('App: Application', () => {
+    it('should have the `defaultMeaning` as 42', inject([ApplicationApp], (app) => {
+        expect(app.defaultMeaning).toBe(42);
     }));
-  });
+
+    describe('#meaningOfLife', () => {
+        it('should get the meaning of life', inject([ApplicationApp], (app) => {
+            expect(app.meaningOfLife()).toBe('The meaning of life is 42');
+            expect(app.meaningOfLife(22)).toBe('The meaning of life is 22');
+        }));
+    });
 });
