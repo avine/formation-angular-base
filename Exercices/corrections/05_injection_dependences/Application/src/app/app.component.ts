42144a0ece51a6f984737fc73ca2c18b207f575a
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2016-09-30 10:31:03 +0200

    Review TP5

---
+++ Exercices/corrections/05_injection_dependences/Application/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, Inject } from '@angular/core';
+import { Product } from './model/product';
+import { ProductService } from './services/product.service';
+import { CustomerService } from './services/customer.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public products:Product[];
+
+  constructor(private productService:ProductService, private customerService:CustomerService, @Inject('welcomeMsg') public title:string){
+      this.products = this.productService.getProducts();
+  }
+
+  getTotal(): number {
+      return this.customerService.getTotal();
+  }
+
+  updatePrice(event) {
+      this.customerService.addProduct(event);
+      this.productService.decreaseStock(event.title);
+  }
+
+  isAvailable(title: string): boolean {
+    return this.productService.isAvailable(title);
+  }
+}
---
+++ Exercices/corrections/05_injection_dependences/Application/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { ProductService } from './services/product.service';
+import { CustomerService } from './services/customer.service';
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+    MenuComponent, 
+    ProductComponent, 
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [ 
+    ProductService, 
+    CustomerService, 
+    {provide: 'welcomeMsg', useValue: 'Bienvenue sur Zenika Ecommerce'}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
