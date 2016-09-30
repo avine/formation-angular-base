b1bb01914d5bb1accd2989c77863fe40d77c1dab
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2016-09-30 13:25:43 +0200

    Review TP6

---
+++ Exercices/corrections/06-pipe/Application/src/app/services/product.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+
+import { Product } from '../model/product';
+
+@Injectable()
+export class ProductService {
+
+  private products: Product[];
+
+  constructor(uppercase:UpperCasePipe) {
+    this.products = new Array<Product>();
+
+    this.products.push(new Product('1Product 1', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 10, 2));
+    this.products.push(new Product('TProduct 2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 20, 2));
+    this.products.push(new Product('ZProduct 3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 30, 2));
+    this.products.push(new Product('BProduct 4', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 40, 2));
+
+    this.products = this.products.map(p => {
+      p.title = uppercase.transform(p.title);
+      return p;
+    })
+  }
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+
+  isTheLast(title: string): boolean {
+    return this.products.find((product) => { return product.title === title }).stock === 1;
+  }
+
+  isAvailable(title: string): boolean {
+    return this.products.find((product) => { return product.title === title }).stock !== 0;
+  }
+
+  decreaseStock(title: string) {
+    this.products.find((product) => { return product.title === title }).stock -= 1;
+  }
+}
