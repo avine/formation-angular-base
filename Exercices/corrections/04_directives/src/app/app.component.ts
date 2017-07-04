99b91083844287717c615a249db1fd3d0c3f713d
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-07-04 20:34:44 +0200

    Solution manquante pour le TP sur les directives

---
+++ Exercices/corrections/04_directives/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { Product } from './model/product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  total:number;
+  products: Product[];
+
+  constructor(){
+    this.total = 0;
+
+    this.products = [];
+    this.products.push(new Product('Product1', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 10, 2));
+    this.products.push(new Product('Product2', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 20, 2));
+    this.products.push(new Product('Product3', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 30, 2));
+    this.products.push(new Product('Product4', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', 'http://placehold.it/800x500', 40, 2));
+    
+  }
+  updatePrice(product:Product){
+    product.stock--;
+    this.total += product.price;
+  }
+}
