7761144d2a94e27334acbfd91f0e1b3734d46c80
Yoann Prot <Taranys@users.noreply.github.com>
2018-12-11 09:50:44 +0100

    :ok_hand: Use the syntaxe keyof Product
    Co-Authored-By: patou <patrice@desaintsteban.fr>

--- Exercices/corrections/06_pipe/src/app/pipes/sort.pipe.ts
+++ Exercices/corrections/06_pipe/src/app/pipes/sort.pipe.ts
@@ -7,7 +7,7 @@ import { Product } from '../model/product';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(value: Product[], column: string = "title"): Product[] {
+  transform(value: Product[], column: keyof Product = "title"): Product[] {
     return [...value].sort((p1, p2) => {
       if (p1[column] > p2[column]) {
         return 1;
