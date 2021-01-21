042826f7d04ccadc589854f64e549778a8c37e5b
Maxime Soulard <max.soulard@gmail.com>
2021-01-21 17:09:28 +0100

    Added a bonus part to forms Exercice on ReactiveForms

---
+++ Exercices/corrections/09_forms_bonus/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { MenuComponent } from './menu/menu.component';
+import { SortPipe } from './pipes/sort.pipe';
+import { HomeComponent } from './home/home.component';
+import { BasketComponent } from './basket/basket.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { BasketGuard } from './basket.guard';
+import { ProductDetailComponent } from './product-detail/product-detail.component'
+
+registerLocaleData(localeFr);
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'product/:id', component: ProductDetailComponent},
+  {path: 'basket', component: BasketComponent, canActivate: [BasketGuard]},
+  {path: '**', redirectTo: 'home'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    MenuComponent,
+    SortPipe,
+    HomeComponent,
+    BasketComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    {provide: 'welcomeMsg', useValue: 'Welcome to Zenika Ecommerces'},
+    {provide: LOCALE_ID, useValue: navigator.language}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
