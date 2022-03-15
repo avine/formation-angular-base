c6ab84928abe9533ea2ad6b003c8b8ed7db124db
Stéphane Francel <stephane.francel@gmail.com>
2022-03-15 12:11:15 +0100

    Run prettier

--- Exercices/solutions/src/app/app.component.spec.ts
+++ Exercices/solutions/src/app/app.component.spec.ts
@@ -24,8 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'This is my first component'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('This is my first component');
   });
 });
