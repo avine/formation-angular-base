ddea496dedc99d5950e9e61e672d01c9a0254fa7
Stéphane Francel <stephane.francel@gmail.com>
2022-03-15 15:15:42 +0100

    Migrate lab 06

--- Exercices/solutions/projects/06_pipe/src/app/app.component.ts
+++ Exercices/solutions/projects/06_pipe/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { Product } from './model/product';
+import { CustomerService } from './services/customer.service';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '06_pipe';
+  products: Product[];
+  sortKey: keyof Product = 'title';
+
+  constructor(
+    private productService: ProductService,
+    private customerService: CustomerService,
+    @Inject('welcomeMsg') public title: string
+  ) {
+    this.products = productService.getProducts();
+  }
+
+  getTotal(): number {
+    return this.customerService.getTotal();
+  }
+
+  updatePrice(event: any) {
+    this.customerService.addProduct(event);
+    this.productService.decreaseStock(event);
+  }
+
+  isAvailable(product: Product): boolean {
+    return this.productService.isAvailable(product);
+  }
 }
