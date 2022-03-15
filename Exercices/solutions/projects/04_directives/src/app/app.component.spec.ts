084e02f7c221b3b4e0f44f356b44fe39495b872a
Stéphane Francel <stephane.francel@gmail.com>
2022-03-15 15:51:26 +0100

    Migrate lab 04

--- Exercices/solutions/projects/04_directives/src/app/app.component.spec.ts
+++ Exercices/solutions/projects/04_directives/src/app/app.component.spec.ts
@@ -1,29 +1,89 @@
-import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed, waitForAsync } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
+import { Product } from './model/product';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  it(
+    'should create the app',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    })
+  );
 
-  it(`should have as title '04_directives'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('04_directives');
-  });
+  it(
+    'should have a total starting at 0',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      expect(app.total).toEqual(0);
+    })
+  );
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('04_directives app is running!');
-  });
+  it(
+    'should have the total bound in the header',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      const compiled = fixture.debugElement.nativeElement;
+
+      app.total = 42;
+      fixture.detectChanges();
+      expect(compiled.querySelector('header').textContent).toContain(42);
+    })
+  );
+
+  it(
+    'should update price with the product price',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+
+      const product = new Product('', '', '', 666, 2);
+      app.total = 42;
+      app.updatePrice(product);
+      expect(app.total).toBe(42 + 666);
+      expect(product.stock).toBe(1);
+    })
+  );
+
+  it(
+    'should bind each product component with its product',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      const compiled = fixture.debugElement.nativeElement;
+
+      fixture.detectChanges();
+      const products = compiled.querySelectorAll('app-product');
+      products.forEach((product: any, i: number) => {
+        expect(product.data).toBe(app.products[i]);
+      });
+    })
+  );
+
+  it(
+    'should not display product with an empty stock',
+    waitForAsync(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.debugElement.componentInstance;
+      const compiled = fixture.debugElement.nativeElement;
+
+      app.products = [new Product('empty', '', '', 42, 0), new Product('available', '', '', 42, 10)];
+      fixture.detectChanges();
+      const products = compiled.querySelectorAll('app-product');
+      expect(products.length).toBe(1);
+      expect(products[0].data).toBe(app.products[1]);
+    })
+  );
 });
--- Exercices/solutions/projects/04_directives/src/app/app.module.ts
+++ Exercices/solutions/projects/04_directives/src/app/app.module.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ProductComponent, MenuComponent],
+  imports: [BrowserModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
